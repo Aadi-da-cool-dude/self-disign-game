d0e9511f139c8564d24c15e9858703d8a1f10bd6
Aadi-da-cool-dude <70690984+Aadi-da-cool-dude@users.noreply.github.com>
2022-04-09 18:31:14 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12
+,wall13,wall14,wall15,wall16,wall17,wall18,wall19,wall20,wall21,wall22
+
+var hunter
+var man
+
+function preload() {
+  
+}
+
+
+function setup() {
+  createCanvas(600,800);
+  wall1 = createSprite(75,150,230,25)
+  wall2 = createSprite(200,112.5,25,100)
+  wall3 = createSprite(400,75,398,25)
+  wall4 = createSprite(430,135,25,100)
+  wall5 = createSprite(510,172.5,180,25)
+  wall6 = createSprite(105,275,360,25)
+  wall7 = createSprite(275,237.5,25,100)
+  wall8 = createSprite(530,275,175,25)
+  wall9 = createSprite(430,316.5,25,108)
+  wall10 = createSprite(220,380,445,25)
+  wall11 = createSprite(500,500,400,25)
+  wall12 = createSprite(312.5,550,25,100)
+  wall13 = createSprite(500,600,400,25)
+  wall14 = createSprite(156,550,325,25)
+}
+
+
+function draw() 
+{
+  background(51);
+  drawSprites()
+  
+}
+
